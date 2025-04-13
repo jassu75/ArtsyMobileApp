7ae1ce4cda70da08f3313acdbae3b774b3807e4f
Tejas Vinayaka Rao Kangod <kangod@usc.edu>
2025-04-13 09:31:03 -0700

    initial commit

---
+++ app/src/main/java/com/example/artsymobileapp/components/App.kt
@@ -0,0 +1,11 @@
+package com.example.artsymobileapp.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    Text(
+        text = "Hello Android!",
+    )
+}
